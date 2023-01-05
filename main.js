ba8edddae1225d2616c4ec65449b4941646f0b22
OuiDesign <74764691+OuiDesign@users.noreply.github.com>
2023-01-05 13:20:12 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Cmok-ZMh4B-Awpe0/scene.splinecode');
